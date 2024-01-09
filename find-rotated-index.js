a46abcd8468c16d9ebe345b216102a88b715bd0f
camdentadhg <camdent@gmail.com>
2024-01-09 16:38:27 -0600

    find rotated index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,46 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, val) {
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
+    let rotateIndex;
+    while (leftIndex <= rightIndex){
+        let middleIndex = Math.floor((rightIndex + leftIndex)/2);
+        if (arr[middleIndex] < arr[middleIndex + 1]){
+            rotateIndex = middleIndex;
+        }
+        else if (arr[middleIndex] < arr[0]){
+            leftIndex = middleIndex + 1;
+        }
+        else {
+            rightIndex = middleIndex - 1;
+        }
+    }
+    leftIndex = 0;
+    rightIndex = rotateIndex
+    while (leftIndex <= rightIndex){
+        middleIndex = Math.floor((leftIndex + rightIndex)/2);
+        if (arr[middleIndex] === val){
+            return middleIndex;
+        }
+        else if (arr[middleIndex] > val){
+            rightIndex = middleIndex - 1;
+        }
+        else 
+            leftIndex = middleIndex + 1;
+    }
+    leftIndex = rotateIndex;
+    rightIndex = arr.length - 1;
+    while (leftIndex <= rightIndex){
+        middleIndex = Math.floor((leftIndex + rightIndex)/2);
+        if (arr[middleIndex] === val){
+            return middleIndex;
+        }
+        else if (arr[middleIndex] > val){
+            rightIndex = middleIndex - 1;
+        }
+        else 
+            leftIndex = middleIndex + 1;
+    }
+    return -1
 }
 
 module.exports = findRotatedIndex
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,40 @@
-function sortedFrequency() {
-
+function sortedFrequency(arr, val) {
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
+    while (leftIndex <= rightIndex){
+        let middleIndex = Math.floor((leftIndex + rightIndex)/2)
+        if (arr[middleIndex] === val && arr[middleIndex-1] !== val){
+            startIndex = middleIndex;
+        }
+        else if (arr[middleIndex] < val){
+            leftIndex = middleIndex + 1;
+        }
+        else {
+            rightIndex = middleIndex - 1;
+        }
+    }
+    console.log('startIndex is ', startIndex);
+    leftIndex = startIndex;
+    rightIndex = arr.length - 1;
+    while (leftIndex <= rightIndex){
+        middleIndex = Math.floor((leftIndex + rightIndex)/2)
+        if (arr[middleIndex] === val && arr[middleIndex + 1] !== val){
+            endIndex = middleIndex;
+        }
+        else if (arr[middleIndex] > val){
+            rightIndex = middleIndex - 1;
+        }
+        else {
+            leftIndex = middleIndex + 1;
+        }
+    }
+    console.log('endIndex is ', endIndex)
+    if (!startIndex && !endIndex){
+        return -1;
+    }
+    else {
+        return endIndex - startIndex + 1;
+    }
 }
 
 module.exports = sortedFrequency
