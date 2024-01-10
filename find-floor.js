51ce15248bf828d7813dd170a01983c39d82b8d6
camdentadhg <camdent@gmail.com>
2024-01-10 15:24:33 -0600

    find floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,34 @@
-function findFloor() {
-  
+function findFloor(arr, val) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+  while (leftIndex <= rightIndex){
+    middleIndex = Math.floor((leftIndex + rightIndex)/2);
+    if (arr[middleIndex] === val){
+        return arr[middleIndex];
+    }
+    else if (arr[middleIndex] > val){
+        if (middleIndex === 0){
+            return -1;
+        }
+        else if (arr[middleIndex - 1] > val){
+            rightIndex = middleIndex - 1;
+        }
+        else {
+            return arr[middleIndex - 1];
+        }
+    }
+    else {
+        if (middleIndex === arr.length - 1){
+            return arr[middleIndex];
+        }
+        else if (arr[middleIndex + 1] === val){
+            return arr[middleIndex + 1];
+        }
+        else {
+            leftIndex = middleIndex + 1;
+        }
+    }
+  }
 }
 
 module.exports = findFloor
